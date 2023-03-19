a708592fd6ee8c60daebbad38c45199ccf4ba7ef
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-19 20:22:37 +0200

    Add project files.

---
+++ Grades/Grades.cs
@@ -0,0 +1,20 @@
+﻿namespace Grades
+{
+    internal class Grades
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            Grade(grade);
+        }
+
+        static void Grade(double grade)
+        {
+            if      (grade >= 5.50 && grade <= 6.00) { Console.WriteLine("Excellent"); }
+            else if (grade >= 4.50 && grade <= 5.49) { Console.WriteLine("Veery good"); }
+            else if (grade >= 3.50 && grade <= 4.49) { Console.WriteLine("Good"); }
+            else if (grade >= 3.00 && grade <= 3.49) { Console.WriteLine("Poor"); }
+            else if (grade >=2.00 && grade <= 2.99) { Console.WriteLine("Fail"); }
+        }
+    }
+}
---
+++ PintSign/PintSign/PrintSign.cs
@@ -0,0 +1,24 @@
+﻿namespace PrintSign
+{
+    internal class PrintSign
+    {
+        static void Main(string[] args)
+        {
+            int number =int.Parse(Console.ReadLine());
+            PrintSing(number);
+            //PrintEvenOrOdd(number);
+        }
+
+        static void PrintSing(int number) 
+        {
+            if (number < 0) { Console.WriteLine($"The number {number} is negative."); }
+            else if (number > 0) { Console.WriteLine($"The number {number} is positive."); }
+            else { Console.WriteLine($"The number {number} is zero."); }
+        }
+        //static void PrintEvenOrOdd(int number)
+        //{
+        //    if (number % 2 == 0) { Console.WriteLine($"{number} is even."); }
+        //    else { Console.Write($"{number} is odd."); }
+        //}
+    }
+}
