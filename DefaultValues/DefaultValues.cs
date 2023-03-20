39b71ffb26bb863118291afe42fa1fb854350b3a
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-20 08:52:35 +0200

    Add project

---
+++ DefaultValues/DefaultValues.cs
@@ -0,0 +1,32 @@
+﻿namespace DefaultValues
+{
+    internal class DefaultValues
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input start number");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input end number");
+            int end = int.Parse(Console.ReadLine());
+            PrintNumber(start, end);
+        }
+        static void PrintNumber(int start,int end) 
+        {
+            int counter = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (i == end)
+                {
+                    Console.Write($"{i}");
+                }
+                else 
+                { Console.Write($"{i}, "); } 
+
+                counter++;
+            }
+            Console.WriteLine(  );
+            Console.WriteLine($"Count number is:{counter}");
+        }
+
+    }
+}
