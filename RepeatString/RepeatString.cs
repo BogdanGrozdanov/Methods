06efa25fb144ef98240245fed8207ec73775d52d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-20 20:49:01 +0200

    add project Repeat string

---
+++ RepeatString/RepeatString.cs
@@ -0,0 +1,27 @@
+﻿namespace RepeatString
+{
+    internal class RepeatString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text");
+            string text = Console.ReadLine();
+            Console.WriteLine("Input number repeat");
+            int num = int.Parse(Console.ReadLine());
+
+            PrintString(text, num);
+        }
+
+        public static void PrintString(string text, int num)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < num; i++)
+            {
+                result += text;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
