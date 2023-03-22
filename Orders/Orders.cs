c1c5f0517a408dda85d616e5fb9fde27c936d79a
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-22 19:39:04 +0200

    Create Orders.cs

---
+++ Orders/Orders.cs
@@ -0,0 +1,38 @@
+﻿namespace Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input : coffe,water,coke or snacks");
+            string order = Console.ReadLine().ToLower();
+            Console.WriteLine("Quantity :");
+            double quantity = int.Parse(Console.ReadLine());
+
+            if (order == "coffee") { Console.WriteLine(Coffee(quantity)); ; }
+
+
+
+        }
+       static private double Coffee(double quantity)
+        {
+            double totalPrice = quantity * 1.5;
+            return totalPrice;
+        }
+        static void Water(int quantity)
+        {
+            double totalPrice = quantity * 1.00;
+            Console.WriteLine(quantity);
+        }
+        static void Coke(int quantity)
+        {
+            double totalPrice = quantity * 1.40;
+            Console.WriteLine(quantity);
+        }
+        static void Snacks(int quantity)
+        {
+            double totalPrice = quantity * 2.00;
+            Console.WriteLine(quantity);
+        }
+    }
+}
