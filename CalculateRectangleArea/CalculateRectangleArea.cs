04d36436f87f0ccc794fee60342af43bb79b34ca
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-22 20:31:16 +0200

    Update CalculateRectangleArea.cs

--- CalculateRectangleArea/CalculateRectangleArea.cs
+++ CalculateRectangleArea/CalculateRectangleArea.cs
@@ -1,15 +1,21 @@
-﻿namespace CalculateRectangleArea
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculateRectangleArea
 {
     internal class CalculateRectangleArea
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("width");
             double width = double.Parse(Console.ReadLine());
             Console.WriteLine("height");
             double height = double.Parse(Console.ReadLine());
             double area = GetRectangleArea(width, height);
             Console.WriteLine(area);
+            
+
         }
 
         static double GetRectangleArea(double width, double height)
