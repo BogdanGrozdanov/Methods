847cb0f3f105a159a60a416199f4423805e5eb8f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-03-22 21:33:54 +0200

    code

--- MathPower/MathPower.cs
+++ MathPower/MathPower.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Input number");
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input Power");
+            int pow = int.Parse(Console.ReadLine());
+            double result = Math.Pow(number, pow);
+            Console.WriteLine(result);
+
+        }
+        static double RiseToPower(double number, int power)
+        {
+            return Math.Pow(number, power);
         }
     }
 }
