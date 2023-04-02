53b456b75145d16b708405e3b74d87e91999f451
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-04-02 20:56:19 +0300

    add some code

--- GreaterOfTwoValues/GreaterOfTwoValues.cs
+++ GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Input type value:");
+            string typeValue = Console.ReadLine().ToLower();
+
+            Console.WriteLine("First value:");
+            string value1 = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Second value:");
+            string value2 = Console.ReadLine().ToLower();
+
+            Console.WriteLine("The greater value is:");
+        }
+
+        internal static string GetMax(string typeValue, string value1, string value2)
+        {
+            string result = string.Empty;
+            if (typeValue == "int")
+            {
+                int firstValue = int.Parse(value1);
+                int secondValue = int.Parse(value2);
+                result = (Math.Max(firstValue, secondValue)).ToString();
+            }
+            else if (typeValue == "char")
+            { 
+                //char firstChar=char.Parse(value1);
+                //char secondChar=char.Parse(value2);
+                //result =Char.GetNumericValue(value1)
+            }
+            return result.ToString();
         }
     }
 }
