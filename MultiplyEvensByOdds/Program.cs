fe9c788587248d5862301ff832522a08daaa1ea9
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-04-10 22:07:22 +0300

    Update Program.cs

--- MultiplyEvensByOdds/Program.cs
+++ MultiplyEvensByOdds/Program.cs
@@ -7,12 +7,20 @@
             Console.WriteLine("Input number : ");
             int number =int.Parse(Console.ReadLine());
             GetSumOfEvenDigits(number);
+
         }
 
         private static int GetSumOfEvenDigits(int number)
         {
             var result = 0;
+            int digit;
+            while (number > 0) 
+            {
+            digit = number % 10;
+                if (digit == 0) { }
+            }
 
+            
 
             return result;
         }
